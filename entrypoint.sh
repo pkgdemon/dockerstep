925efaf2eb3e2e77a63ca6508896380bee3c9e85
pkgdemon <pkgdemonteam@gmail.com>
2024-02-10 20:08:06 -0500

    Add important GNUstep default for running without WM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Source GNUstep.sh
 . /System/Library/Makefiles/GNUstep.sh
+
+# Write GNUstep defaults
+defaults write NSGlobalDomain GSBackHandlesWindowDecorations NO
 
 # Start GWorkspace with XWayland
 exec openapp GWorkspace --display ":0"

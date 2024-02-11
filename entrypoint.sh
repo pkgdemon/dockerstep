86ffa40322263640e71cd0f95ea81412f5a45c39
pkgdemon <pkgdemonteam@gmail.com>
2024-02-10 19:41:35 -0500

    Remove defaults from entrypoint as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Check if GNUstep directory exists in user's home
-if [ ! -d "/home/$USER/GNUstep" ]; then
-    # If not, copy GNUstep defaults recursively from /etc/skel
-    cp -r /etc/skel/GNUstep /home/$USER/
-fi
-
 # Start GWorkspace with XWayland
 exec GWorkspace --display ":0"
